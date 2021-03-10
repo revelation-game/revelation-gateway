c341cce755e29859e353e6af3427a66f6f52a583
Simon Hauck <simonhauck@gmx.de>
2021-03-11 00:00:29 +0100

    try to fix github action

---
+++ src/test/java/io/github/revelationgame/gateway/GatewayApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.revelationgame.gateway;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GatewayApplicationTest {
+
+    @Test
+    public void helloWorld() {
+        Assertions.assertThat(true).isTrue();
+    }
+}

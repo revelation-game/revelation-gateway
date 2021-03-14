212a164a617122fb7489476264554dd3df5f3fb5
Simon Hauck <simonhauck@gmx.de>
2021-03-14 11:48:40 -0700

    add swagger implementation for gateway

---
+++ src/main/java/io/github/revelationgame/gateway/logging/RequestLoggerFilter.java
@@ -0,0 +1,45 @@
+package io.github.revelationgame.gateway.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class RequestLoggerFilter implements GlobalFilter, Ordered {
+
+    private static long REQUEST_ID = 0;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String remoteUrl = Optional.ofNullable(exchange.getRequest().getRemoteAddress())
+                .map(InetSocketAddress::getAddress)
+                .map(InetAddress::getHostAddress)
+                .orElse("Failed to find removeAddress");
+
+        long requestID = REQUEST_ID++;
+        long startTime = System.currentTimeMillis();
+
+        log.info("Incoming request, ID = {}, Origin: {}, URL: {} - {}", requestID, remoteUrl, exchange.getRequest().getMethodValue(), exchange.getRequest().getURI().toString());
+
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> {
+                    long endTime = System.currentTimeMillis();
+
+                    log.info("Request finished, ID = {}, Required Time: {}ms", requestID, endTime - startTime);
+                }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
---
+++ src/main/java/io/github/revelationgame/gateway/openapi/SwaggerUiConfig.java
@@ -0,0 +1,42 @@
+package io.github.revelationgame.gateway.openapi;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springdoc.core.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@RestController
+@ConfigurationProperties(prefix = "revelation.custom-swagger")
+public class SwaggerUiConfig {
+
+    @Getter
+    @Setter
+    private String hostName;
+
+    @Getter
+    @Setter
+    private List<String> urlPrefixes;
+
+    @Value("${server.port}")
+    private String applicationPort;
+
+
+    @GetMapping("/swagger-config.json")
+    public Map<String, Object> swaggerConfig() {
+
+        List<SwaggerUrl> urls = urlPrefixes.stream()
+                .map(prefix -> new SwaggerUrl(prefix, "http://localhost:" + applicationPort + "/" + prefix + "/custom-open-api/v3/api-docs?serverUrl=" + hostName + "/" + prefix))
+                .collect(Collectors.toList());
+
+        return Map.of("urls", urls);
+    }
+}
